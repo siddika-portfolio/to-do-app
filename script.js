ff009aee2485298200c92c5c652fcafbf708e83c
Siddika-akhter <siddika.tas28@gmail.com>
2021-04-19 11:19:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
+
+form.addEventListener('submit', (e) => {
+     e.preventDefault();
+     const todoText = input.value;
+     if (todoText) {
+         const todoEl = document.createElement
+         ("li");
+         todoEl.innerText = todoText;
+         todos.appendChild(todoEl);
+
+         input.value = "";
+     }
+ });
